a3cf01ae8ce0cf82f05629191625cf0f7c72f929
krosh22 <Olga2018r@yandex.ru>
2021-04-08 15:48:45 +0300

    add js burger

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 const sectionBtn = document.getElementById('section-btn');
+const menu_2 = document.getElementById("menu_2")
 
 sectionBtn.addEventListener('click', function (e) {
     e.preventDefault();
-    this.classList.toggle('menu-btn')
+    console.log('click',menu_2)
+    menu_2.classList.remove('menu_2')
 });
+
+const closed = document.getElementById("closed")
+closed.addEventListener("click", function (e) {
+    e.preventDefault();
+    menu_2.classList.add('menu_2')
+});
+
